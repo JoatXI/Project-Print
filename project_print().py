8e7481a9040ff37b7b26df84c8dd44908b3c87e4
Michael Paterson <michaelpaterson194@gmail.com>
2023-03-15 22:08:29 +0000

    modified the wake mode function

--- project_print().py
+++ project_print().py
@@ -14,9 +14,9 @@ engine = pyttsx3.init()
 voices = engine.getProperty("voices")
 engine.setProperty("voice", voices[1].id)
 
-wake_commands = ["hello print", "are you there print", "print are you there", "ok print", "hey print", "excuse me print", "yo print", "do you have a moment print", "can you help me with this print", "can you help me out with this print", "print, can you help me with this", "print can you help me out with this", "what's up print", "print what's up" "check this out print", "print check this out", "can you listen to this print", "print can you listen to this", "listen to this print", "print listen to this", "wake up print", "print wake up"]
+wake_commands = ["hello print", "are you there print", "print are you there", "ok print", "hey print", "excuse me print", "yo print", "do you have a moment print", "can you help me with this print", "can you help me out with this print", "print can you help me with this", "print can you help me out with this", "what's up print", "print what's up" "check this out print", "print check this out", "can you listen to this print", "print can you listen to this", "listen to this print", "print listen to this", "wake up print", "print, wake up"]
 
-wake_response = ["hello, how may I help you", "hello, what's up", "hi, how can I help you?", "hello, what do you need?"]
+wake_response = ["hello, how may I help you", "hello, what's up", "hi, how can I help you?", "hello, what do you need?", "hey, what can I do for you?"]
 
 unknown_command_response = ["sorry, I did not get that", "sorry, can you repeat that?", "I didn't hear that clearly", "say that again please", "beg your pardon, could you repeat that?", "that was unclear, can you speak more clearly?"]
 
@@ -31,7 +31,7 @@ def taking_voice_commands():
         with sr.Microphone() as source:
             print("listening...")
             listener.adjust_for_ambient_noise(source)
-            voice = listener.listen(source)
+            voice = listener.listen(source, timeout=60)
             command = listener.recognize_google(voice)
             command = command.lower()
 # print() being the name of this AI (Note: should progamme AI with the abililty to change it's name through user command)
@@ -66,7 +66,7 @@ def wake_mode():
         with sr.Microphone() as voice_source:
             print("listening in wake mode...")
             listener.adjust_for_ambient_noise(voice_source)
-            voice = listener.listen(voice_source)
+            voice = listener.listen(voice_source, phrase_time_limit=3)
             alert_command = listener.recognize_google(voice)
             alert_command = alert_command.lower()
 
