99626608b1c41c78a84ab809534d59cf467cb696
Michael Paterson <michaelpaterson194@gmail.com>
2023-03-15 21:08:35 +0000

    modified the wake mode function

--- project_print().py
+++ project_print().py
@@ -4,10 +4,10 @@ import wikipedia
 import pywhatkit
 import datetime
 import pyjokes
-import selenium
 import random
 import time
 import pyautogui
+import selenium
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -20,6 +20,8 @@ wake_response = ["hello, how may I help you", "hello, what's up", "hi, how can I
 
 unknown_command_response = ["sorry, I did not get that", "sorry, can you repeat that?", "I didn't hear that clearly", "say that again please", "beg your pardon, could you repeat that?", "that was unclear, can you speak more clearly?"]
 
+search_queries = ["who is", "tell me about", "what do you know about", "who the hell is", "do a search on"]
+
 def speak(text):
     engine.say(text)
     engine.runAndWait()
@@ -28,20 +30,22 @@ def taking_voice_commands():
     try:
         with sr.Microphone() as source:
             print("listening...")
+            listener.adjust_for_ambient_noise(source)
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
 # print() being the name of this AI (Note: should progamme AI with the abililty to change it's name through user command)
     except:
         pass
-    return command
+        return None
+    else:
+        return command
 
 def run_print():
     voice_command = taking_voice_commands()
     print(voice_command)
     if 'google' in voice_command:
         question = voice_command.replace('google', '')
-        #question = voice_command
         info = wikipedia.summary(question, 1)
         print(info)
         speak(info)
@@ -55,27 +59,27 @@ def run_print():
     elif 'joke' in voice_command:
         speak(pyjokes.get_joke())
     else:
-        speak('sorry I did not get that')
+        speak(random.choice(unknown_command_response))
         
 def wake_mode():
     try:
         with sr.Microphone() as voice_source:
             print("listening in wake mode...")
+            listener.adjust_for_ambient_noise(voice_source)
             voice = listener.listen(voice_source)
             alert_command = listener.recognize_google(voice)
             alert_command = alert_command.lower()
+
+            if alert_command in wake_commands:
+                print(alert_command)
+                speak(random.choice(wake_response))
+                while True:
+                    run_print()
     except:
         pass
-    
-    #voice_alert = taking_voice_commands()
-    print(alert_command)
-    if alert_command in wake_commands:
-        speak(random.choice(wake_response))
-        while True:
-            run_print()
+        return None
     else:
-        speak(random.choice(unknown_command_response))
-    return alert_command
+        return alert_command
       
         
 while True:
